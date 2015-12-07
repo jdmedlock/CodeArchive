a8f1f05101aec5e32155a777c7935a410976bb44
Jim Medlock <jdmedlock@gmail.com>
2015-12-06 20:51:23 -0600

    Completed testSolution method and started printSolution method.

--- JavaCodeArchive/src/com/relaxedcomplexity/EightQueens.java
+++ JavaCodeArchive/src/com/relaxedcomplexity/EightQueens.java
@@ -26,33 +26,34 @@ package com.relaxedcomplexity;
  * <ul>
  * <li>The state of the board is maintained as an 8x8 matrix where
  *     each position may have one of the values:
- *       - Unoccupied: '-'
- *       - Occupied:   'Q'
- *       - Blocked:    '_'
+ *       <ul>
+ *       <li>Unoccupied: '-'
+ *       <li>Occupied:   'Q'
+ *       </ul>
  * <li>Two additional arrays of 8 cells each are maintained which
  *     maintain the state of each row and column of the board. This
  *     state indicates whether a Queen has been placed anywhere in 
  *     that row or column and removes the need to check each position
  *     in a row or column to determine if positioning a new Queen is
  *     blocked. These arrays may have one of two values:
- *       - Unoccupied: false
- *       - Occupied:   true
+ *       <ul>
+ *       <li>Unoccupied: false
+ *       <li>Occupied:   true
+ *       </ul>
  * <li>Before a Queen can be placed on the board the following conditions
  *     must be met:
- *       - The positions row must not already contain a Queen (see #2)
- *       - The positions column must not already contain a Queen (see #2)
- *       - The diagonal(s) must not already contain a Queen. For a given 
- *         position the diagonals are defined as the positions with the
- *         coordinates:
- *           - (r+1,c-1) : Only rows 1 thru 7 and columns 2 thru 8
- *           - (r-1,c-1) : Only rows 2 thru 8 and columns 2 thru 8
- *           - (r-1,c+1) : Only rows 2 thru 8 and columns 1 thru 7
- *           - (r+1,c+1) : Only rows 1 thru 7 and columns 1 thru 7
+ *       <ul>
+ *       <li>The positions row must not already contain a Queen (see #2)
+ *       <li>The positions column must not already contain a Queen (see #2)
+ *       <li>The diagonal(s) must not already contain a Queen. 
+ *       </ul>
  * <li>When a Queen is placed on the board: 
- *  	 - The state of the row and column it occupies is updated (see
+ * 	     <ul>
+ *  	 <li>The state of the row and column it occupies is updated (see
  *         #2).
- *       - Each position on the board along the it's diagonal(s) are
+ *       <li>Each position on the board along the it's diagonal(s) are
  *         updated to indicate that they are blocked.
+ *       </ul>
  * <li>When a solution is found the chessboard will be printed as a line
  *     containing the solution number and the positions (r,c) containing
  *     the eight queens.
@@ -66,9 +67,8 @@ public class EightQueens {
 	private static final int	NCOLS = 8;
 	private static final int	NQUEENS = 8;
 	private static final int	NSOLS = 92;
-	private static final char	UNOCCUPIED = '-';
+	private static final char	UNOCCUPIED = ' ';
 	private static final char	OCCUPIED = 'Q';
-	private static final char 	BLOCKED = '_';
 	
 	private static boolean		solutionFound = false;
 	private static int			solutionCount = 0;
@@ -97,12 +97,59 @@ public class EightQueens {
 	/**
 	 * Test to determine if a Queen can be placed at the current position on the
 	 * chess board.
+	 * <p>
+	 * Before a Queen can be placed on the board the following conditions
+     * must be met:
+     *   <ul>
+     *   <li>The position must not already be occupied by a Queen
+     *   <li>The positions row must not already contain a Queen 
+     *   <li>The positions column must not already contain a Queen
+     *   <li>The diagonal(s) must not already contain a Queen. For a given 
+     *       position the diagonals are defined as the positions with the
+     *       coordinates:
+     *         <ul>
+     *         <li>(r+1,c+1) : Only rows 1 thru 7 and columns 1 thru 7
+     *         <li>(r+1,c-1) : Only rows 1 thru 7 and columns 2 thru 8
+     *         <li>(r-1,c+1) : Only rows 2 thru 8 and columns 1 thru 7
+     *         <li>(r-1,c-1) : Only rows 2 thru 8 and columns 2 thru 8
+     *         </ul>
+     *   </ul>
 	 * 
 	 * @param rowPos Row number of the position coordinate to test
 	 * @param colPos Column number of the position coordinate to test
 	 */
-	public static boolean testPosition(int currRow, int currCol) {
-		return false;
+	public static boolean testPosition(int rowPos, int colPos) {
+		boolean testResult = false;
+		
+		if (chessBoard[rowPos][colPos] == OCCUPIED) {
+			return false;
+		}
+		
+		if (rowOccupied[rowPos]||colOccupied[colPos]) {
+			return false;
+		}
+		
+		if (rowPos < NROWS && colPos < NCOLS) {
+			testResult = testPosition(rowPos+1,colPos+1);
+		}
+		if (rowPos < NROWS && colPos > 1) {
+			testResult |= testPosition(rowPos+1,colPos-1);
+		}
+		if (rowPos > 1     && colPos < NCOLS) {
+			testResult |= testPosition(rowPos-1,colPos+1);
+		}
+		if (rowPos > 1     && colPos > 1) {
+			testResult |= testPosition(rowPos-1,colPos-1);
+		}
+		
+		return testResult;
+	}
+	
+	/**
+	 * Print the current solution
+	 */
+	public static void printSolution(solutionNo) {
+		
 	}
 	
 	/**
