985e5682d7d567e9b21754a591b0ee8b629ff025
Jim Medlock <jdmedlock@gmail.com>
2015-12-10 14:07:58 -0600

    Commit to fix GIT local directory rename

---
+++ CodeArchive/src/com/relaxedcomplexity/devicecntl/DeviceCntl.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.relaxedcomplexity.devicecntl;
+
+/**
+ * This program demonstrates how to control devices by controlling the 
+ * pitch and volume of a sound on the local computer using the mouse. 
+ * Scrolling up and down adjusts the volume while moving the mouse left 
+ * and right adjusts the pitch of the sound. 
+ * 
+ * Since two discrete "things" must interact this is an interesting 
+ * example of how to use design patterns to both simplify the code and 
+ * make it easily maintainable and extensible.
+ * 
+ * @author jim.medlock
+ *
+ */
+public class DeviceCntl {
+
+	/**
+	 * @param args An array of parameters to be used by the program. However,
+	 * this are not necessary for this program and are ignored.
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
