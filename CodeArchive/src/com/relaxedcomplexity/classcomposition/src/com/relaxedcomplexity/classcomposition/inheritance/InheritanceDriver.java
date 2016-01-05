2d4cf66099a922cd0a2c095dbc0a7886a493ea39
Jim Medlock <jdmedlock@gmail.com>
2016-01-04 19:48:03 -0600

    Added minimal-json jar

--- CodeArchive/src/com/relaxedcomplexity/classcomposition/src/com/relaxedcomplexity/classcomposition/inheritance/InheritanceDriver.java
+++ CodeArchive/src/com/relaxedcomplexity/classcomposition/src/com/relaxedcomplexity/classcomposition/inheritance/InheritanceDriver.java
@@ -3,6 +3,14 @@
  */
 package com.relaxedcomplexity.classcomposition.inheritance;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import com.eclipsesource.json.Json;
+import com.eclipsesource.json.JsonArray;
+import com.eclipsesource.json.JsonObject;
+
 /**
  * @author Jim Medlock
  *
@@ -13,8 +21,21 @@ public class InheritanceDriver {
    * @param args
    */
   public static void main(String[] args) {
-    Desktop myDesktopComputer = new Desktop();
+	  try {
+        FileReader reader = new FileReader("MyFile.txt");
+        BufferedReader bufferedReader = new BufferedReader(reader);
+        JsonObject object = Json.parse(input).asObject();
 
+        String line;
+
+        while ((line = bufferedReader.readLine()) != null) {
+              System.out.println(line);
+        }
+        reader.close();
+
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
   }
 
 }
